f192ac010392e67a2fe826de4aa80b1f9f58540b
Metehan İNAL <metehan.inal25@gmail.com>
2019-11-19 21:44:20 +0300

    eklendi
    deneme 1

---
+++ Loops.java
@@ -0,0 +1,27 @@
+package ud1;
+
+public class Loops {
+	public static void main(String[] args ) {
+		
+		/*int  myNumbers [] = {12,15,18,21,24};
+		
+		for (int i = 0; i<myNumbers.length; i++) {
+			
+			int x = myNumbers [i]/3*5 ;
+			System.out.println(x);
+			
+			
+		}
+		*/
+		// for ( int number : myNumbers) {sout(number);} bu da yeni bir değişken ile yapılmış hali. 
+		
+		int k = 0;
+		while (k<5) {
+			int x = k*10;
+			System.out.println(x);
+			k++;
+		
+		
+	}
+	}
+}
---
+++ fgh.java
@@ -0,0 +1,15 @@
+package ud1;
+
+public class fgh {
+
+	public static void main(String[] args) {
+		int x = 5 ;
+		x++;
+		System.out.println(x);
+		int y = 8 ;
+		System.out.println(x<y);
+		
+		
+		
+	}
+}
